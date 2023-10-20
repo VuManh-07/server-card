682806d421571a4a809eb2cc918c08bd033e93e7
VuManh-07 <manhvd1507@gmail.com>
2023-10-20 12:16:23 +0700

    update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const url_ptit = process.env.URL_PTIT || "http://localhost:3040"
+const url_ptit = process.env.URL_PTIT || "http://139.162.47.179:3040"
 
 const api = axios.create({
     baseURL: url_ptit,
